2416eb5bcc41a5846eed9a2f4b618879011bddad
Haoshu Duan <hsduan2023@outlook.com>
2024-01-01 13:28:31 -0500

    update key values

--- atus_shinyapp.R
+++ atus_shinyapp.R
@@ -19,10 +19,11 @@ library(shiny)
 library(shinyWidgets)
 library(dslabs)
 library(plotly)
+library(rsconnect)
 
 
-setwd("/Users/haoshu/Desktop/atus/")
-getwd()
+#setwd("/Users/haoshu/Desktop/atus/")
+#getwd()
 
 
 # data("us_contagious_diseases")
@@ -32,7 +33,25 @@ getwd()
 #              names_to = "data", values_to = "value")
 # head(disease)
 # Obtain grouped time-use data 
-data <-readRDS(paste0('data_save/', 'grouped_time.rds'))
+
+#data <-readRDS(paste0('data_save/', 'grouped_time.rds'))
+data <-readRDS('grouped_time.rds') %>%
+        mutate(category = case_when(category == 'wrk_edu' ~ 'work and eduction',
+                                    category == 'self_care' ~ 'self care',
+                                    category == 'child_care' ~ 'child care',
+                                    category == 'adult_care' ~ 'care for adults',
+                                    category == 'house_wrk' ~ 'housework',
+                                    category == 'exec' ~ 'exercise',
+                                    category == 'phone_fam' ~ 'calling family members',
+                                    category == 'relig' ~ 'religious activities', 
+                                    category == 'sed_act' ~'sedentary activities',
+                                    category == 'soc_leis' ~ 'socializing leisure time',
+                                    category == 'soc_serv' ~ 'volunteering services',
+                                    category == 'travel' ~ 'traveling/communiting', 
+                                    category == 'wrk_act' ~ 'work activities', 
+                                    category == 'sleep' ~ 'sleep', 
+                                    category == 'ind_leis' ~ 'lesisure time alone',
+                                    TRUE ~ NA_character_))
 
 head(data)
 category_choice<-unique(data$category)
@@ -51,7 +70,7 @@ ui <- fluidPage(
                                        selected="Female", multiple =FALSE), 
                         checkboxGroupInput("CategoryInput", "Category",
                                            choices = category_choice,
-                                           selected = c("wrk_edu", "house_wrk")),
+                                           selected = c("work and eduction", "housework")),
                         sliderInput("YearInput", "Year", min=2003, max=2022, 
                                     value=c(2003, 2022), sep=""),
                         # radioGroupButtons("dataInput", "Data",
